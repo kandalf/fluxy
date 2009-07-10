04f27378849c975281ad93b4495e5952e4d96a35
Leonardo Mateo <lmateo@gisworking.com>
2009-07-10 17:52:41 -0300

    Env var changed and README edited

--- fluxy.rb
+++ fluxy.rb
@@ -9,9 +9,9 @@ include FireWatir
 @browser = Firefox.new
 @browser.goto("file:///#{File.expand_path(File.dirname(__FILE__))}/base_test.html")
 
-sleep(3)
+#sleep(3)
 
 puts @browser.execute_script "document.getElementById('testDiv').innerHTML = 'set from ruby';"
 
 puts @browser.execute_script("testCall()")
-@browser.close unless ENV["WATIR_KEEP_OPEN"]
+@browser.close unless ENV["FLUXY_KEEP_OPEN"]
